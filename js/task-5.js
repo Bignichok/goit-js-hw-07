786669d82fa519d9adae35093f8c9ef307df3b80
Nikita-Samoilenko <mykyta.samoilenko@gmail.com>
2024-09-24 14:57:55 +0300

    task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,18 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const refs = {
+        changeColorButton: document.querySelector(".change-color"),
+        color: document.querySelector(".color"),
+    };
+
+    refs.changeColorButton.addEventListener("click", () => {
+        const bgColor = getRandomHexColor();
+        document.body.style.backgroundColor = bgColor;
+        refs.color.textContent = bgColor;
+    });
+});
